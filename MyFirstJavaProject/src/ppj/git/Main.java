202f2d53a6a54a26d427e70582613cd6f746675b
mmachnow <machnowski@mail.com>
2019-10-10 14:35:34 +0200

    Moj pierwszy program zapisany w Git

---
+++ MyFirstJavaProject/src/ppj/git/Main.java
@@ -0,0 +1,11 @@
+package ppj.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Java project with Git");
+
+	}
+
+}
