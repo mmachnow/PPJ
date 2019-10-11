b69187d894d70b649c7eda96ab4cd8a2cd2e95b7
mmachnow <machnowski@mail.com>
2019-10-11 16:17:37 +0200

    Tablica int

---
+++ tmp/src/tmp/Main.java
@@ -0,0 +1,16 @@
+package tmp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] tab = new int [1000][4];
+	    for (int w = 0; w < tab.length; w++) {
+	        for (int k = 0; k < tab[w].length; k++) {
+
+	            System.out.print((tab[w][k]) + ", ");
+	        }
+	        System.out.println("\n");
+	    }
+	}
+}
